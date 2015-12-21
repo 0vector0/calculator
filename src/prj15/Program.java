f3755fc8b7acba15579545554da4a3a3adadff6b
0vector0 <0vector0@gmail.com>
2015-12-22 01:47:02 +0200

    new

---
+++ src/prj15/Program.java
@@ -0,0 +1,12 @@
+package prj15;
+
+import javax.swing.JFrame;
+
+public class Program {
+
+	public static void main(String[] args) {
+		JFrame myWindow = new SimpleWindow();
+		myWindow.setVisible(true);
+	}
+
+}
